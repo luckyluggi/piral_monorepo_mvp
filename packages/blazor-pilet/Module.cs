5415983e50c6cbfec47f3bef2cb6548e362d68e7
Lukas Eiter <lukas.eiter@casablanca.at>
2022-01-18 09:38:20 +0100

    add blazor pilet

---
+++ packages/blazor-pilet/Module.cs
@@ -0,0 +1,12 @@
+public class Module
+{
+    public static void Main()
+    {
+        // this entrypoint should remain empty
+    }
+
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        // configure dependency injection here
+    }
+}
